88da0570d72feee3c3865e40053f186bfe7ab7ad
Filipe Melo <filipemelo032@gmail.com>
2020-06-22 15:44:35 -0300

    build shell script

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
+echo 'INSTALLING ADONIS'
+npm i -g @adonisjs/cli
 echo 'RUNNING MIGRATIONS'
-npx adonis migration:run
+adonis migration:run
 echo 'SERVING APPLICATION'
-npx adonis serve
+adonis serve
